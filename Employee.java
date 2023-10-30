e8633a87997b7cf9586c7fc050e0b79297e8bc4a
insifa0 <89314837+insifa0@users.noreply.github.com>
2023-10-30 15:28:57 +0000

    little fixing

--- Employee.java
+++ Employee.java
@@ -1,4 +1,9 @@
 // extends Person ==> Customer class ı Person classında olan herşeye sahip. Aralarındaki fark Customer in *start_date* bilgisinin olması.
 class Employee extends Person{
   String start_date;
+
+  public  Member(String tc, String n, String s){
+    // ust sinifin constructorini cagir
+    super(t,n,s);
+  }
 }
--- Member.java
+++ Member.java
@@ -1,3 +1,12 @@
 class Member extends Person{
   Book[] rented_books;
+
+  public  Member(String tc, String n, String s){
+    // ust sinifin constructorini cagir
+    super(t,n,s);
+  }
+
+  public Member(){
+    super("","","");
+  } //Hiç bir değeri olmayan member oluşturmak için
 }
